60d804cf21c69ef50b99dd5eea0db6492797618a
giovannihr <balarezogb@gmail.com>
2017-02-13 10:12:54 -0800

    First Commit

---
+++ app/app.js
@@ -0,0 +1,33 @@
+(function() {
+    'use strict';
+
+    var hackApp = angular.module('hackApp', ['toastr', 'ui.router']);
+
+    hackApp.filter('trustThisUrl', ['$sce', function($sce) {
+        return function(val) {
+            return $sce.trustAsResourceUrl(val);
+        };
+    }]);
+
+    hackApp.config(function($stateProvider, $urlRouterProvider) {
+
+        $urlRouterProvider.otherwise("/linked");
+
+        $stateProvider
+
+
+            .state('linked', {
+                url: "/linked",
+                templateUrl: "app/partials/linked.html",
+                controller: "mainController",
+                controllerAs: "vm"
+
+            })
+            .state('video', {
+                url: "/video",
+                templateUrl: "app/partials/video.html",
+                controller: "indeedController",
+                controllerAs: "vm"
+            });
+    });
+})();
